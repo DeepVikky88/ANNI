a46aa6ac6f42984be2833b4745f26b82243d95f2
𓄂️𝆺𝅥⃝☠️ ⃪ͥ͢ ᷟ 𝐌͢ꝛ♥️꯭⃝⃨⃜⃝𝐕𝐈𝐊𝐊𝐘༏⃪͢🔥᪵᪳❛𝆺꯭𝅥༎ࠫ <vikashyadav735556@gmail.com>
2025-02-12 15:12:51 +0530

    Update start.py

--- PURVIMUSIC/plugins/bot/start.py
+++ PURVIMUSIC/plugins/bot/start.py
@@ -96,15 +96,15 @@ async def start_pm(client, message: Message, _):
                 chat_photo = userss_photo
         chat_photo = userss_photo if userss_photo else START_IMG_URL
 
-    except AttributeError:
+        except AttributeError:
             chat_photo = "assets/nodp.png"
-    await m.delete()
-    await message.reply_photo(
+        await m.delete()
+        await message.reply_photo(
             photo=chat_photo,
             caption=_["start_2"].format(message.from_user.mention, app.mention),
             reply_markup=InlineKeyboardMarkup(out),
-    )
-    if await is_on_off(2):
+        )
+        if await is_on_off(2):
             return await app.send_message(
                 chat_id=config.LOGGER_ID,
                 text=f"{message.from_user.mention} ᴊᴜsᴛ sᴛᴀʀᴛᴇᴅ ᴛʜᴇ ʙᴏᴛ.\n\n<b>ᴜsᴇʀ ɪᴅ :</b> <code>{message.from_user.id}</code>\n<b>ᴜsᴇʀɴᴀᴍᴇ :</b> @{message.from_user.username}",
